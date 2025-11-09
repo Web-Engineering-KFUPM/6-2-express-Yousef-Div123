fa1886f6af16451af04aa6a0ae65893b28df40c0
Yousef Mahmoud <mryoyo133@gmail.com>
2025-11-09 12:05:30 +0300

    submission

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
+const app = express();
+const PORT = 3000;
 
 {/*write code for cors*/}
+app.use(cors());
 
-
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quote Generator API");
+});
 {/*write code to define routes*/}
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+});
 
 
 {/*write code to create server*/}
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
